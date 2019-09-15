90d3065d8de447dc37e89d0b05474abf4ea79f09
Aliaksei <alexx333by@protonmail.com>
2019-09-15 15:08:48 +0300

    Added HarmonicParameter tuple to simplify polyharmonic generation

--- signal_generators.py
+++ signal_generators.py
@@ -1,15 +1,17 @@
 import math
+from collections import namedtuple
+
+HarmonicParameters = namedtuple('HarmonicParameters', ['amplitude', 'frequency', 'initial_phase'])
 
 
 class HarmonicSignalGenerator:
-    def __init__(self, amplitude, frequency, initial_phase, period):
-        self.__amplitude = amplitude
-        self.__frequency = frequency
-        self.__initial_phase = initial_phase
+    def __init__(self, harmonic_params, period):
+        self.__params = harmonic_params
         self.__period = period
 
     def __get_next_signal_part(self, n):
-        return self.__amplitude * math.sin(2 * math.pi * self.__frequency * n / self.__period + self.__initial_phase)
+        return self.__params.amplitude * math.sin(2 * math.pi * self.__params.frequency * n / self.__period
+                                                  + self.__params.initial_phase)
 
     def get_signal(self):
         for n in range(self.__period):
