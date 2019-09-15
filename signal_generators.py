6c520826342d47033dcb3fa4d94c7bed2eb152c5
Aliaksei <alexx333by@protonmail.com>
2019-09-15 15:39:58 +0300

    Polyharmonic generator implemented

--- signal_generators.py
+++ signal_generators.py
@@ -5,14 +5,24 @@ HarmonicParameters = namedtuple('HarmonicParameters', ['amplitude', 'frequency',
 
 
 class HarmonicSignalGenerator:
-    def __init__(self, harmonic_params, period):
+    def __init__(self, harmonic_params):
         self.__params = harmonic_params
-        self.__period = period
 
-    def __get_next_signal_part(self, n):
-        return self.__params.amplitude * math.sin(2 * math.pi * self.__params.frequency * n / self.__period
+    def get_signal_part(self, n, period):
+        return self.__params.amplitude * math.sin(2 * math.pi * self.__params.frequency * n / period
                                                   + self.__params.initial_phase)
 
-    def get_signal(self):
-        for n in range(self.__period):
-            yield self.__get_next_signal_part(n)
+    def get_signal(self, period):
+        for n in range(period):
+            yield self.get_signal_part(n, period)
+
+
+class PolyHarmonicSignalGenerator:
+    def __init__(self, harmonic_params_collection):
+        self.__harmonic_generators = []
+        for harmonic_params in harmonic_params_collection:
+            self.__harmonic_generators.append(HarmonicSignalGenerator(harmonic_params))
+
+    def get_signal(self, period):
+        for n in range(period):
+            yield sum([generator.get_signal_part(n, period) for generator in self.__harmonic_generators])
